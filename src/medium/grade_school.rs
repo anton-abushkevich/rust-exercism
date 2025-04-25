968a4813b75c123dac875114a265fb6f8a2cf45b
Anton <anton.abushkevich@gmail.com>
2025-04-25 05:05:50 +0300

    change grade_school solution

--- src/medium/grade_school.rs
+++ src/medium/grade_school.rs
@@ -9,7 +9,7 @@ impl School {
     }
 
     pub fn add(&mut self, grade: u32, student: &str) {
-        if !self.0.values().any(|students| students.contains(student)) {
+        if self.0.values().all(|students| !students.contains(student)) {
             self.0.entry(grade).or_default().insert(student.to_string());
         }
     }
