b7d448b523d988fe782b9091185c0a248db1fb07
Anton <anton.abushkevich@gmail.com>
2025-05-12 14:36:24 +0300

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #[allow(unused_imports)]
-use testpr::{easy, medium};
+use testpr::{easy, medium, hard};
 
 fn main() {
     //println!("{:?}", medium::alphametics::solve("AGENT + SMITH == MENACE"));
     //println!("{:?}", medium::alphametics::solve("MORE + LESS == OHYES"));
     //println!("{:?}", medium::alphametics::solve("RED + BLUE == PINK"));
 }
-
